41b7e49bc08b980d6d3d76dcb66c4241d54cc98c
j-berg <j-berg@users.noreply.github.com>
2019-01-19 15:13:04 -0700

    Fixed errors with sample_overview, added pca

--- micartools/analyze.py
+++ micartools/analyze.py
@@ -30,6 +30,7 @@ import matplotlib.patches as patches
 import seaborn as sns
 sns.set(font='arial')
 jakes_cmap = sns.diverging_palette(212, 61, s=99, l=77, sep=1, n=16, center='dark') #Custom aesthetics
+from sklearn.decomposition import PCA
 
 """
 DESCRIPTION: Plots heatmap for prep_data formatted data
@@ -134,28 +135,34 @@ palette= List of colors for samples in order of plotting
 save_fig= If not None, provide full file path, name, and extension to save the file as
 dpi= Set dpi of saved figure
 bbox_inches= Format saved figure (often useful for making sure no text is cut off)
+title= Provide title for figure and saved file if save_fig option used
 
 ASSUMPTIONS:
 Data has been scaled and labeled with the MICARtools prep_data function
 """
 def sample_overview(data_scaled, info , gene_list=None, order=None, palette=None, save_fig=None, dpi=600, bbox_inches='tight', title=None):
 
+    #For some reason, function is returning data_scaled with labels, even though not specified to return anything
+    #This solves the issue, but still throws the error
+    scaled = data_scaled.copy()
+
     #Prep data_scaled by adding labels from info
     labels = pd.Series(info[1].values,index=info[0]).to_dict()
-    data_scaled.loc['label'] = data_scaled.columns.map(labels.get)
+    scaled.loc['label'] = scaled.columns.map(labels.get)
 
     #Output collapsed dataframe
-    newIndex = ['label'] + [ind for ind in data_scaled.index if ind != 'label']
-    data_scaled = data_scaled.reindex(index=newIndex)
+    newIndex = ['label'] + [ind for ind in scaled.index if ind != 'label']
+    scaled = scaled.reindex(index=newIndex)
 
-    data_scaled = data_scaled.T
-    data_scaled = data_scaled.set_index('label', drop=True)
-    data_scaled = data_scaled.T
+    scaled = scaled.T
+    scaled = scaled.set_index('label', drop=True)
+    scaled = scaled.T
+    scaled = scaled.dropna(axis=0)
 
-    data_unstacked = data_scaled.unstack().reset_index()
+    data_unstacked = scaled.unstack().reset_index()
     data_unstacked = data_unstacked.rename(columns={data_unstacked.columns[0]: 'type', data_unstacked.columns[1]: 'gene', data_unstacked.columns[2]: 'expr'})
 
-    #Custom panel heatmap
+    #Custom gene list to grab distributions from
     if gene_list != None:
 
         #Check file formats
@@ -169,24 +176,160 @@ def sample_overview(data_scaled, info , gene_list=None, order=None, palette=None
             print('Incorrect gene_list type provided')
             return
 
-    swarm = data_unstacked.loc[(data_unstacked.ix[:,1].isin(genes))]
+        swarm = data_unstacked.loc[(data_unstacked.iloc[:,1].isin(genes))]
 
+    else:
+        swarm = data_unstacked
+
+    #Final formatting check
+    swarm[["expr"]] = swarm[["expr"]].apply(pd.to_numeric)
+
+    #Plot
     if order is None or palette is None:
         ax = sns.violinplot(x="type", y="expr", data=swarm)
     elif type(order) is list or type(palette) is list:
-        ax = sns.violinplot(x="type", y="expr",
-                        data=swarm, order=order,
-                       palette=palette)
+        ax = sns.violinplot(x="type", y="expr", data=swarm, order=order, palette=palette)
     else:
         return
 
+    ax.set_xlabel('')
+    ax.set_ylabel('Expression')
+
+    #Save fig
     if save_fig is not None:
         if title is not None:
-            ax.set_xlabel('')
-            ax.set_ylabel('Expression')
             plt.savefig('./MICARtools_violin_plot.pdf', dpi=dpi, bbox_inches=bbox_inches)
         else:
-            ax.set_xlabel('')
-            ax.set_ylabel('Expression')
             ax.set_title(str(title))
             plt.savefig(str(save_fig), dpi=dpi, bbox_inches=bbox_inches)
+
+"""
+DESCRIPTION:
+
+METHODS: Confidence interval number to plot (i.e. 1 == CI1 == 68%, 2 == CI2 == 95%, 3 == CI3 == 99%,)
+
+VARIABLES:
+palette= Dictionary of categories and their corresponding plotting colors
+n_components= Number of components to plot on the scree plot
+
+USAGE:
+
+ASSUMPTIONS:
+
+FEATURES TO ADD:
+Allow for compatibility with adding labels for gene classes for plotting
+"""
+def pca(data_scaled, info, palette, grouping='samples', gene_labels=False, ci=2, gene_list=None, save_fig=None, save_scree=None, n_components=10, dpi=600, bbox_inches='tight', title=None):
+
+    if gene_list != None:
+
+        #Check file formats and get dataframe with genes of interest
+        if type(gene_list) is list:
+            data_scaled = data_scaled.reindex[gene_list]
+
+        elif type(gene_list) is str:
+            genes = custom_list(str(gene_list))
+            data_scaled = data_scaled.reindex[genes]
+
+        else:
+            print('Incorrect gene_list type provided')
+            return
+
+    #Clean data
+    data_scaled = data_scaled.dropna(axis=0)
+
+    #Format data for sample-wise or gene-wise PCA analysis
+    if str(grouping) == 'samples':
+        data_scaled = data_scaled.T
+
+    elif str(grouping) == 'genes':
+        data_scaled = data_scaled
+
+    else:
+        print('Incorrect grouping variable provided')
+        return
+
+    #Prep PCA
+    pca = PCA(n_components=n_components)
+    pca_result = pca.fit_transform(data_scaled.values)
+
+    #Record PCs
+    x = 1
+    while x <= n_components:
+        pc = 'PC' + str(x)
+        col = x - 1
+        data_scaled[pc] = pca_result[:,col]
+
+    #Scree
+    vari = 'Explained variation per principal component: {}'.format(np.round(pca.explained_variance_ratio_, decimals=4)*100)
+    scree = np.round(pca.explained_variance_ratio_, decimals=4)*100
+
+    if save_scree != None:
+
+        #Plot scree
+        sing_vals = np.arange(n_components) + 1
+
+        ax = sns.lineplot(x=sing_vals, y=scree, color="red")
+        ax.set(xlabel='Principal Component', ylabel='Proportion of Variance Explained', title='Scree Plot')
+        plt.savefig(str(save_fig[:-4]) + '_scree.pdf', dpi=dpi, bbox_inches=bbox_inches)
+
+        #Remove scree from memory to prevent plot bleeding
+        del ax
+        plt.close()
+        plt.clf()
+
+    if gene_labels == False:
+
+        #Prep data_scaled by adding labels from info to column (samples are rows)
+        labels = pd.Series(info[1].values,index=info[0]).to_dict()
+
+        data_scaled['label'] = data_scaled.index.to_series().map(labels)
+
+        #Plot PC1 & PC2
+        df_pca = data_scaled[['PC1','PC2','label']] #Prepare pca data
+        unique_labels = df_pca['label'].unique() #Gather unique labels
+        pca_plot = sns.scatterplot(df_pca.PC1, df_pca.PC2, hue=df_pca['label'], palette=palette)
+
+        #Add confidence intervals
+        color_counter = 0
+        for x in unique_labels:
+
+            #slice df into label specific datasets
+            df_slice = df_pca[df_pca['label'] == x]
+
+            #make numpy array from label-specific dfs
+            x_slice = df_slice.PC1.values
+            y_slice = df_slice.PC2.values
+
+            #pca maths
+            cov = np.cov(x_slice, y_slice)
+            lambda_, v = np.linalg.eig(cov)
+            theta = np.degrees(np.arctan2(*v[:,0][::-1]))
+            lambda_ = np.sqrt(lambda_)
+
+            #plot
+            pca_plot.add_patch(patches.Ellipse(xy=(np.mean(x_slice), np.mean(y_slice)),
+                              width=lambda_[0]*ci*2, height=lambda_[1]*ci*2,
+                              angle=theta,
+                              alpha=0.3, facecolor=palette[x], edgecolor='black', linewidth=1, linestyle='solid')
+                              )
+
+            color_counter += 1
+
+        # Put the legend out of the figure
+        handles,labels = pca_plot.get_legend_handles_labels()
+        handles = [handles[1], handles[3], handles[2]]
+        labels = [labels[1],labels[3],labels[2]]
+
+        plt.legend(handles,labels, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        plt.xlabel('PC1 (' + str(round(scree[0],2)) + '%)')
+        plt.ylabel('PC2 (' + str(round(scree[1],2)) + '%)')
+
+        if save_fig != None:
+
+            #Save plot
+            plt.title(str(title))
+            plt.savefig(str(save_fig), dpi=dpi, bbox_inches=bbox_inches)
+
+    else:
+        print('This feature has not been implemented yet')
