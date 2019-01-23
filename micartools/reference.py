afa430f2b1e4270d3e48fa4a25a5fc80a04a4b4e
j-berg <j-berg@users.noreply.github.com>
2019-01-23 12:01:25 -0700

    Added gene_dictionary related functions

--- micartools/reference.py
+++ micartools/reference.py
@@ -38,10 +38,16 @@ METHODS: Considers strandedness
 Multiprocesses chunks of a dataframe on cores of computer
 
 VARIABLES:
+gtf_file= GTF reference file path and name
+truncate= Number of nucleotides to truncate from the 5' end of each exon 1 (considers strandedness)
+save_file= Save truncated coding only GTF reference to file path and name
+save_coding= Save coding only GTF reference to file path and name
+return_dataframe= Return the truncated, coding only table when running the function
 
 ASSUMPTIONS:
+Input file is a properly formatted GTF file
 """
-def truncate_gtf(gtf_file, truncate=45, save_file=None, save_coding_only=None, delimiter='\t', return_dataframe=False):
+def truncate_gtf(gtf_file, truncate=45, save_file=None, save_coding=None, return_dataframe=False):
 
     #Import gtf reference file to
     if str(args_dict['input']).endswith('.gtf'):
@@ -54,8 +60,8 @@ def truncate_gtf(gtf_file, truncate=45, save_file=None, save_coding_only=None, d
     gtf_coding = gtf[gtf.iloc[:, 8].str.contains('protein_coding') == True]
 
     #Save to .gtf file (tsv)
-    if save_coding_only != None:
-        gtf_coding.to_csv(str(save_coding_only), sep=delimiter, header=None, index=False, quoting=csv.QUOTE_NONE)
+    if save_coding != None:
+        gtf_coding.to_csv(str(save_coding), sep='\t', header=None, index=False, quoting=csv.QUOTE_NONE)
 
     gtf_coding_c = gtf_coding.copy()
 
@@ -66,4 +72,110 @@ def truncate_gtf(gtf_file, truncate=45, save_file=None, save_coding_only=None, d
         return gtf_truncated
 
     if save_file != None:
-        gtf_truncated.to_csv(str(save_file), sep=delimiter, header=None, index=False, quoting=csv.QUOTE_NONE)
+        gtf_truncated.to_csv(str(save_file), sep='\t', header=None, index=False, quoting=csv.QUOTE_NONE)
+
+"""
+DESCRIPTION: Create a dictionary of gene IDs and common names from a GTF file
+
+METHODS: Can be customized to create dictionary from any dataframe
+
+VARIABLES:
+file= Reference file path and name (IMPORTANT: If providing a csv or tsv, must be header-less)
+file_type= Default 'gtf', other options: 'csv', 'tsv'
+return_dictionary
+save_file= Save reference dictionary as a dataframe to file path and name (Only used for file_type='gtf')
+gene_id= Position or column where gene_id information is found (default set for gtf)
+gene_name= Position or column where gene_name information is found (default set for gtf)
+
+ASSUMPTIONS:
+Input file is a properly formatted GTF file
+"""
+def gene_dictionary(file, file_type='gtf', return_dictionary=True, save_file=None, gene_id=0, gene_name=2):
+
+    #Create dictionary from gtf file
+    if file_type == 'gtf':
+        gtf = pd.read_csv(str(file), sep="\t", header=None, comment='#', low_memory=False)
+
+        #Get gene record rows from gtf
+        gtf_genes = gtf.loc[gtf[2] == 'gene']
+
+        #From the gene info column, split out gene_id and gene_name
+        gtf_genes['gene_id'] = gtf[8].str.split(';').str[gene_id]
+        gtf_genes['gene_name'] = gtf[8].str.split(';').str[gene_name]
+        gtf_genes['gene_id'] = gtf_genes['gene_id'].map(lambda x: x.lstrip('gene_id \"').rstrip('\"').rstrip(' '))
+        gtf_genes['gene_name'] = gtf_genes['gene_name'].map(lambda x: x.lstrip('gene_name \"').rstrip('\"').rstrip(' '))
+
+        #Create dictionary
+        dict_df = gtf_genes[['gene_id','gene_name']].copy()
+        gene_dict = pd.Series(gtf_genes.gene_id.values, index=gtf_genes.gene_name).to_dict()
+
+        if save_file != None:
+            dict_df.to_csv(str(save_file), sep='\t', index=False)
+
+        if return_dictionary == True:
+            return gene_dict
+
+    #Create dictionary from dataframe where columns of file are gene_id and gene_name
+    elif file_type == 'csv' or file_type == 'tsv':
+        if file_type == 'csv':
+            delimiter = ','
+            data = pd.read_csv(str(file), sep=delimiter, header=None, comment='#', low_memory=False)
+        elif file_type == 'tsv':
+            delimiter = '\t'
+            data = pd.read_csv(str(file), sep=delimiter, header=None, comment='#', low_memory=False)
+        else:
+            print('Error: Invalid file_type input')
+
+        #Make sure file doesn't contain any spaces in names or IDs
+        data[gene_id] = data[gene_id].map(lambda x: x.rstrip(' '))
+        data[gene_name] = data[gene_name].map(lambda x: x.rstrip(' '))
+
+        #Create dictionary
+        dict_df = data[[gene_id,gene_name]].copy()
+        dict_df.columns = ['gene_id','gene_name']
+        gene_dict = pd.Series(data.gene_id.values, index=data.gene_name).to_dict()
+
+        if save_file != None:
+            dict_df.to_csv(str(save_file), sep=delimiter, index=False)
+
+        if return_dictionary == True:
+            return gene_dict
+
+    else:
+        print('Error: Invalid input provided')
+        return
+
+"""
+DESCRIPTION:
+
+VARIABLES:
+gene_dictionary= MICARtools formatted gene_dictionary returned from gene_dictionary()
+data= Dataframe where gene_id or gene_name is in a column
+gene_id= Column number (int) or header name (str) in data of gene_id if converting gene_id to gene_name
+gene_name= Column number (int) or header name (str) in data of gene_name if converting gene_name to gene_id
+
+IMPORTANT:
+gene_id and gene_name should not be used together (one should be None)
+
+ASSUMPTIONS:
+No column is provided as index
+"""
+def apply_dictionary(gene_dictionary, data, gene_id=None, gene_name=None, return_data=True, save_file=None, delimiter=','):
+
+    data_c = data.copy()
+
+    if gene_id == None and gene_name != None:
+        data_c[gene_name] = data_c[gene_name].map(lambda x: x.rstrip(' '))
+        data_c['gene_id'] = data_c[gene_name].map(gene_dictionary)
+    elif gene_name == None and gene_id != None:
+        data_c[gene_id] = data_c[gene_id].map(lambda x: x.rstrip(' '))
+        data_c['gene_name'] = data_c[gene_id].map(gene_dictionary)
+    else:
+        print('Error: Invalid gene_id or gene_name input provided')
+        return
+
+    if return_data == True:
+        return data_c
+
+    if save_file != None:
+        data_c.to_csv(str(save_file), sep=delimiter, index=False)
