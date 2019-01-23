26a9aa80a16f2e1e74a4166877b33f21affce600
j-berg <j-berg@users.noreply.github.com>
2019-01-23 11:22:54 -0700

    Added parallelize() and truncate() for use in reference.py

--- micartools/utils.py
+++ micartools/utils.py
@@ -37,3 +37,43 @@ def custom_list(file):
             gene_list = line.split(",")
 
     return gene_list
+
+"""
+DESCRIPTION:
+"""
+#Truncate 45 nt
+def truncate(gtf):
+    gtf['plus'] = gtf[[2,3,4,6,8]].apply(lambda x:
+        (x[3] + 45) if x[2] == "exon" and x[3] + 45 <= x[4] and x[6] == "+" and "exon_number \"1\"" in x[8] else (
+        "delete_this" if x[2] == "exon" and x[3] + 45 > x[4] and x[6] == "+" and "exon_number \"1\"" in x[8] else x[3]),axis=1)
+
+    gtf['minus'] = gtf[[2,3,4,6,8]].apply(lambda x:
+        (x[4] - 45) if x[2] == "exon" and x[3] <= x[4] - 45 and x[6] == "-" and "exon_number \"1\"" in x[8] else (
+        "delete_this" if x[2] == "exon" and x[3] > x[4] - 45 and x[6] == "-" and "exon_number \"1\"" in x[8] else x[4]),axis=1)
+
+    #remove exon1s that are too short
+    gtf = gtf[~gtf['plus'].isin(['delete_this'])]
+    gtf = gtf[~gtf['minus'].isin(['delete_this'])]
+
+    #copy new coordinates back to original columns
+    gtf[3] = gtf['plus']
+    gtf[4] = gtf['minus']
+
+    #remove placeholder columns
+    gtf = gtf.drop(columns=['plus','minus'])
+    return gtf
+
+"""
+DESCRIPTION: Parallelize function on a chunk of a dataframe
+"""
+def parallelize(data, func):
+
+    cores = cpu_count() #Number of CPU cores on your system
+    partitions = cpu_count() #Define as many partitions as you want
+
+    data_split = np.array_split(data, partitions)
+    pool = Pool(cores)
+    data = pd.concat(pool.map(func, data_split))
+    pool.close()
+    pool.join()
+    return data
